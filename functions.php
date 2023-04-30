69946412519d9056d58f61234651e52edbffbd12
Matt Wiebe <wiebe@automattic.com>
2023-04-30 11:02:52 -0500

    continuing to iterate, we'll call this a savepoint

--- functions.php
+++ functions.php
@@ -1,7 +1,17 @@
 <?php
 
-function my_theme_scripts() {
+function mw_theme_scripts() {
+	// rough security: only when logged-in
+	if ( ! is_user_logged_in() ) {
+		return;
+	}
 	$index_asset = require( get_stylesheet_directory() . '/build/index.asset.php' );
 	wp_enqueue_script( 'poaster', get_stylesheet_directory_uri() . '/build/index.js', $index_asset['dependencies'], $index_asset['version'], true );
+	//add wp_inline_script with login and password constants
+	$login_vars = [];
+	$login_vars['identifier'] = defined( 'POASTER_LOGIN' ) ? POASTER_LOGIN : '';
+	$login_vars['password'] = defined( 'POASTER_PASSWORD' ) ? POASTER_PASSWORD : '';
+	$script = sprintf( 'window._poasterLogin = %s;', json_encode( $login_vars ) );
+	wp_add_inline_script( 'poaster', $script, 'before' );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'mw_theme_scripts' );
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import { BskyAgent } from "@atproto/api";
+import { BskyAgent } from '@atproto/api';
+import { TextareaControl } from '@wordpress/components';
+
+const getAgent = async () => {
+	const agent = new BskyAgent({ service: 'https://bsky.social/' });
+	await agent.login( window._poasterLogin );
+	return agent;
+}
+
+async function main() {
+	let agent;
+	await getAgent().then( result => agent = result );
+
+	agent.getTimeline({ limit: 10 }).then( result => console.log( 'getTimeline', result ) );
+}
+
+main();
+
+console.log( TextareaControl );
